6b934784db4390c6f94e00a110900f5582662939
Xebec13 <dhoesen@gmail.com>
2025-08-27 20:59:00 +0200

    30%

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-
+import { Navbar, Header, AboutUs, SpecialMenu } from "./components";
+import "./App.css";
 function App() {
-  
-
   return (
     <>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Header } from "./Header/Header";
+export { default as AboutUs } from "./AboutUs/AboutUs";
+export { default as SpecialMenu } from "./SpecialMenu/SpecialMenu"
